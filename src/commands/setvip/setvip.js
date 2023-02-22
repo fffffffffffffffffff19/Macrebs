b5985740b6c065460bc91593cc5c302d3f3356f0
-ffffffffff <davischiavon21@gmail.com>
2023-02-22 11:13:32 -0300

    fix on setVip command

--- src/commands/setvip/setvip.js
+++ src/commands/setvip/setvip.js
@@ -18,7 +18,7 @@ module.exports = {
         const vip = await interaction.options.getString('vips');
         const vipTime = await interaction.options.getString('time');
         const logChannel = await interaction.guild.channels.cache.get('917393598415249468');
-        const vipRole = await interaction.guild.roles.cache.find((role) => role.name == vip);
+        const vipRole = await interaction.guild.roles.cache.find((roleFind) => roleFind.name == vip);
 
         const Time = () => {
             const date = new Date();
@@ -27,9 +27,9 @@ module.exports = {
             return times;
         };
 
-        await interaction.reply({ embeds: [InteraReply(member, logChannel)], ephemeral: true });
+        await vipStatusDB.create({ userId: member.id, vipRole: vipRole.name, giver: interaction.user.id, timeCreated: Time().timeNow, timeExpired: Time().timeSet });
         await member.roles.add(vipRole);
         await logChannel.send({ embeds: [VipLog(member, vipRole, Time().timeNow, Time().timeSet)] });
-        await vipStatusDB.create({ User_ID: member.id, VipRole: vipRole.name, Giver: interaction.user.id, Time_Created: Time().timeNow, Time_Expired: Time().timeSet });
+        await interaction.reply({ embeds: [InteraReply(member, logChannel)], ephemeral: true });
     },
 };
